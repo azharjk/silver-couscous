f4c4e23241dc9e411458618d5de1d64d6452175e
azharjk <azhr.project@gmail.com>
2022-07-15 19:25:57 +0700

    Add a pokemon list get from PokeAPI

--- App.tsx
+++ App.tsx
@@ -1,6 +1,16 @@
 import * as React from "react";
 
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  ListRenderItem,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+
+import type { Pokemon } from "./interfaces/pokemon";
+import { usePokemonFromApi } from "./services/pokemon";
 
 const styles = StyleSheet.create({
   safeAreaContainer: {
@@ -19,14 +29,32 @@ const styles = StyleSheet.create({
     marginTop: 6,
     color: "#808080",
   },
+  pokemon: {
+    marginTop: 20,
+  },
 });
 
+const renderPokemonItem: ListRenderItem<Pokemon> = ({ item }) => {
+  return (
+    <Text>
+      {item.name} {item.url}
+    </Text>
+  );
+};
+
 export default function App() {
+  const pokemon = usePokemonFromApi();
+
   return (
     <SafeAreaView style={styles.safeAreaContainer}>
       <View style={styles.header}>
         <Text style={styles.header__title}>Silver Couscous</Text>
         <Text style={styles.header__desc}>Pokemon App using PokeAPI</Text>
+        <FlatList
+          style={styles.pokemon}
+          data={pokemon}
+          renderItem={(ctx) => renderPokemonItem(ctx)}
+        />
       </View>
     </SafeAreaView>
   );
---
+++ services/pokemon.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+import type { Pokemon, PokemonResponse } from "../interfaces/pokemon";
+
+export const usePokemonFromApi = () => {
+  const [pokemon, setPokemon] = useState<Pokemon[]>();
+
+  const getPokemonFromApiImpl = async () => {
+    try {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
+      const json = (await response.json()) as PokemonResponse;
+      setPokemon(json.results);
+    } catch (e) {
+      console.error("[ERROR]: getPokemonFromApi: ", e);
+    }
+  };
+
+  useEffect(() => {
+    getPokemonFromApiImpl();
+  });
+
+  return pokemon;
+};
